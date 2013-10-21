1f472391f72cd6c69af4f50a76e0ed3bd7b5bda0
Thomas Weinert <thomas@weinert.info>
2013-10-21 19:56:38 +0200

    Support a second (reverse direction pin) for H bridge setups

--- src/Carica/Chip/Motor.php
+++ src/Carica/Chip/Motor.php
@@ -8,34 +8,60 @@ namespace Carica\Chip {
 
     private $_speedPin = NULL;
     private $_directionPin = NULL;
+    private $_reverseDirectionPin = NULL;
 
-    public function __construct(Firmata\Pin $speedPin, Firmata\Pin $directionPin = NULL) {
+    public function __construct(
+      Firmata\Pin $speedPin,
+      Firmata\Pin $directionPin = NULL,
+      Firmata\Pin $reverseDirectionPin = NULL) {
       $this->_speedPin = $speedPin;
       $this->_directionPin = $directionPin;
+      $this->_reverseDirectionPin = $reverseDirectionPin;
+
     }
 
     public function forward($speed) {
+      $this->setPinModes();
       if (NULL !== $this->_directionPin) {
-        $this->_directionPin->mode = Firmata\Board::PIN_MODE_OUTPUT;
         $this->_directionPin->digital = FALSE;
+        if (NULL !== $this->_reverseDirectionPin) {
+          $this->_directionPin->digital = !$this->_directionPin->digital;
+        }
       }
-      $this->_speedPin->mode = Firmata\Board::PIN_MODE_PWM;
       $this->_speedPin->analog($speed);
     }
 
     public function backward($speed) {
+      $this->setPinModes();
       if (NULL == $this->_directionPin) {
-        throw new LogicException('No direction pin provided.');
+        throw new LogicException('No direction pin(s) provided.');
       }
-      $this->_directionPin->mode = Firmata\Board::PIN_MODE_OUTPUT;
       $this->_directionPin->digital = TRUE;
-      $this->_speedPin->mode = Firmata\Board::PIN_MODE_PWM;
+      if (NULL !== $this->_reverseDirectionPin) {
+        $this->_directionPin->digital = !$this->_directionPin->digital;
+      }
       $this->_speedPin->analog($speed);
     }
 
     public function stop() {
-      $this->_speedPin->mode = Firmata\Board::PIN_MODE_PWM;
+      $this->setPinModes();
       $this->_speedPin->analog(0);
+      if (NULL !== $this->_directionPin) {
+        $this->_directionPin->digital = FALSE;
+        if (NULL !== $this->_reverseDirectionPin) {
+          $this->_directionPin->digital = FALSE;
+        }
+      }
+    }
+
+    private function setPinModes() {
+      $this->_speedPin->mode = Firmata\Board::PIN_MODE_PWM;
+      if (NULL !== $this->_directionPin) {
+        $this->_directionPin->mode = Firmata\Board::PIN_MODE_OUTPUT;
+      }
+      if (NULL !== $this->_reverseDirectionPin) {
+        $this->_directionPin->mode = Firmata\Board::PIN_MODE_OUTPUT;
+      }
     }
 
   }
