0552170c4594578cc03dc6b8c2750d9c85504bb3
Thomas Weinert <thomas@weinert.info>
2016-07-20 09:13:58 +0200

    'More displays and cleanup'

--- examples/gpio/matrix/scrollx.php
+++ examples/gpio/matrix/scrollx.php
@@ -1,18 +1,17 @@
 <?php
 $board = require(__DIR__.'/../bootstrap.php');
 
-$max = new Carica\Chip\Max7219\Matrix(
-  new \Carica\Gpio\ShiftOut(
-    $board->pins[38],
-    $board->pins[40],
-    $board->pins[36]
-  )
+use \Carica\Chip\Max7219;
+use \Carica\Gpio\ShiftOut;
+
+$max = new Max7219\Matrix(
+  new ShiftOut($board->pins[38], $board->pins[40], $board->pins[36])
 );
-$displayCount = 1;
+$displayCount = 6;
 for ($i = 0; $i < $displayCount; $i++) {
-  $max->addDisplay($i * 8, 0);
+  $max->addDisplay($i * 8, 0, Max7219\Matrix\Display::ROTATION_CCW_270);
 }
 $image = imagecreatefrompng(__DIR__.'/elephpant-animation.png');
-$max->scrollX($image, -17, 250)->brightness(0.5)->on();
+$max->scrollX($image, -17, 100)->brightness(0.5)->on();
 
 Carica\Io\Event\Loop\Factory::run();
